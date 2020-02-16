c567e4f9cca7e7fbe1be440b920b204e16b70f74
Иннокентий Борисоглебский <i.borisoglebskiy@outlook.com>
2020-02-16 20:47:56 +0300

    ViewModel project was added. BTSContext was added to ViewModel.

---
+++ BTSViewModel/BTSContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTreeSearch;
+
+namespace BTSViewModel
+{
+    public class BTSContext: NotifyPropertyBase
+    {
+        private BinaryTree _binaryTree;
+
+        public BTSContext()
+        {
+            _binaryTree = new BinaryTree();
+        }
+
+        public BinaryTree BinaryTree
+        {
+            get => _binaryTree;
+            set
+            {
+                if (value != _binaryTree)
+                {
+                    _binaryTree = value;
+                    OnPropertyChanged("BinaryTree");
+                }
+            }
+        }
+    }
+}
